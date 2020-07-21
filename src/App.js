1cdbe75d5532ca260bedf696b7e54fdade0ebc3a
claudio scotto <claudioscotto@macbook-pro.mynet>
2020-07-21 18:25:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, {useState, useEffect} from 'react';
+import {overview, followers, totalFollowers} from './data'
+import Switch from "react-switch";
+import IconDown from './images/icon-down.svg'
+import IconUp from './images/icon-up.svg'
+import './App.scss';
+
+function App() {
+
+  const [theme, setTheme] = useState('dark')
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme])
+
+  return (
+      
+      <div className='container'>
+        <div className='row align-center heading-section'>
+          <div className='col'>
+              <h1>Social Media Dashboard</h1>
+              <div className='subheader'>Total Followers: {totalFollowers}</div>
+          </div>
+          <div className='col'>
+          <div className='switcher-wrapper'>
+              <span>Dark mode</span>
+              <Switch onChange={() => setTheme((t) => t === 'light' ? 'dark' : 'light')} checked={theme === 'light'} 
+                width={48} height={24} 
+                handleDiameter={18}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                className={`switcher ${theme === 'light' ? 'unchecked' : 'checked'}`}
+              />
+          </div>
+          </div>
+        </div>
+        <div className='row' id={'social-media'}>
+         {followers.map((data, i) => {
+            return (
+                 <div className='col' key={i}>
+                    <div className={`card ${data.social}`}>
+                        <div className='card-row'>
+                          <span className='icon'><img src={data.icon} alt={data.social}/></span>
+                          <span className='username'>{data.username}</span>
+                        </div>
+                        <div className='followers'>{data.followers}</div>
+                        <div className='desc'>{data.kind}</div>
+                        <div className={`analysis ${data.analysis[0] === '+' ? 'up' : 'down'}`}>
+                        <img src={data.analysis[0] === '+' ? IconUp : IconDown} alt={data.analysis[0] === '+' ? 'arrow up' : 'arrow down'} />
+                        {data.analysis.substr(1)} Today
+                        </div>
+                    </div>
+                </div>
+              )
+         })}
+        </div>
+        <div className='row'>
+          <div className='col'>
+              <h2>Overview - Today</h2>
+          </div>
+        </div>
+        <div className='row' id={'overview'}>
+         {overview.map((data, i) => {
+            return (
+                 <div className='col-4' key={i}>
+                    <div className={`card ${data.social}`}>
+                        <div className='card-row align-center space-between'>
+                          <span className='subject'>{data.subject}</span>
+                          <span className='icon'><img src={data.icon} alt={data.social}/></span>
+                        </div>
+                        <div className='card-row align-baseline space-between'>
+                          <div className='data'>{data.data}</div>
+                          <div className={`analysis ${data.analysis[0] === '+' ? 'up' : 'down'}`}>
+                            <img src={data.analysis[0] === '+' ? IconUp : IconDown} alt={data.analysis[0] === '+' ? 'arrow up' : 'arrow down'} />
+                            {data.analysis.substr(1)}
+                          </div>
+                        </div>
+                    </div>
+                </div>
+              )
+         })}
+        </div>
+      </div>
+
+  );
+
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,100 @@
+import IconFacebook from './images/icon-facebook.svg'
+import IconTwitter from './images/icon-twitter.svg'
+import IconInstagram from './images/icon-instagram.svg'
+import IconYoutube from './images/icon-youtube.svg'
+
+export const totalFollowers = '23,004';
+
+export const followers = [
+	{
+		username:'@nathanf',
+	  	followers:'1987',
+	  	kind:'followers',
+	  	analysis:'+12',
+	  	social:'facebook',
+	  	icon: IconFacebook,
+	},
+	{
+		username:'@nathanf',
+	  	followers:'1044',
+	  	kind:'followers',
+	  	analysis:'+99',
+	  	social:'twitter',
+	  	icon: IconTwitter,
+	},
+	{
+		username:'@nathanf',
+	  	followers:'11k',
+	  	kind:'followers',
+	  	analysis:'+1099',
+	  	social:'instagram',
+	  	icon: IconInstagram,
+	},
+	{
+		username:'@nathanf',
+	  	followers:'8239',
+	  	kind:'subscribers',
+	  	analysis:'-144',
+	  	social:'youtube',
+	  	icon: IconYoutube,
+	}
+];
+
+export const overview = [
+	{
+		subject:'Page Views',
+		social:'facebook',
+		icon: IconFacebook,
+	  	data:'87',
+	  	analysis:'+3%'
+	},
+	{
+		subject:'Likes',
+		social:'facebook',
+		icon: IconFacebook,
+	  	data:'52',
+	  	analysis:'-2%'
+	},
+	{
+		subject:'Page Views',
+		social:'instagram',
+		icon: IconInstagram,
+	  	data:'5462',
+	  	analysis:'+2257%'
+	},
+	{
+		subject:'Profile Views',
+		social:'instagram',
+		icon: IconInstagram,
+	  	data:'52k',
+	  	analysis:'+1375%'
+	},
+	{
+		subject:'Retweets',
+		social:'twitter',
+		icon: IconTwitter,
+	  	data:'117',
+	  	analysis:'+303%'
+	},
+	{
+		subject:'Likes',
+		social:'twitter',
+		icon: IconTwitter,
+	  	data:'507',
+	  	analysis:'+553%'
+	},
+	{
+		subject:'Likes',
+		social:'youtube',
+		icon: IconYoutube,
+	  	data:'107',
+	  	analysis:'-19%'
+	},
+	{
+		subject:'Total Views',
+		social:'youtube',
+		icon: IconYoutube,
+	  	data:'1047',
+	  	analysis:'-12%'
+	},
+]
